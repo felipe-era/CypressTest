842dbb7ca2d0c7d81130ebbd386506e1cbd96f57
feveloper <felip@FEVOLEPER>
2024-02-25 12:29:53 -0300

    Mocha Up

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   video: true,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
